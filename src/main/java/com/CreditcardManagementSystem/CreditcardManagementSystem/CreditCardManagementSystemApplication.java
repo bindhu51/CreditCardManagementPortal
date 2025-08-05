026b8bc6d01253201969be424ea8f76d30537057
bindhus <kothakotas@zeta.tech>
2025-08-05 14:52:47 +0530

    changes

--- src/main/java/com/CreditcardManagementSystem/CreditcardManagementSystem/CreditCardManagementSystemApplication.java
+++ src/main/java/com/CreditcardManagementSystem/CreditcardManagementSystem/CreditCardManagementSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreditcardManagementSystemApplication {
+public class CreditCardManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreditcardManagementSystemApplication.class, args);
+		SpringApplication.run(CreditCardManagementSystemApplication.class, args);
 	}
 
 }
