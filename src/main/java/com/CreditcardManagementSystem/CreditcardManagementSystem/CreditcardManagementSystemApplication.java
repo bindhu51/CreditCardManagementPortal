96affef059c7945ab241d9aad78005cbbc3a9a06
bindhus <kothakotas@zeta.tech>
2025-08-05 14:42:54 +0530

    Spring intialiser

---
+++ src/main/java/com/CreditcardManagementSystem/CreditcardManagementSystem/CreditcardManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CreditcardManagementSystem.CreditcardManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardManagementSystemApplication.class, args);
+	}
+
+}
